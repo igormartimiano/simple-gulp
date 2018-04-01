9bca1bb36485adec6304ae1934d5c7d904eff6e3
Igor Martimiano <igor.martimiano@gmail.com>
2018-04-01 11:10:57 -0300

    ESLint added

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "rules": {
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
